545888c9a48fca08343234345bfc8f3bb23ebc3e
jinhui_LEE <105095093+Ljinyh@users.noreply.github.com>
2022-06-27 17:56:15 +0900

    사용자 메일 인증번호 발급 추가
    사용자가 회원가입 시 입력한 이메일로 인증번호 발급하는 기능 추가

--- controller/userController.js
+++ controller/userController.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken')
 const userDB = require('../models/user')
 const Joi = require('joi')
 const bcrypt = require('bcrypt')
+const mailer = require('../models/mail');
 
 require('dotenv').config()
 
@@ -38,7 +39,7 @@ exports.signUp = async(req, res) => {
 
     try {
         const { userId, email, nickName, phoneNum, userLocation, favorability, password, confirmPassword } =
-        await UserSchema.validateAsync(req.body)
+        await UserSchema.validateAsync(req.body);
 
         if (password !== confirmPassword) {
             return res
@@ -48,11 +49,11 @@ exports.signUp = async(req, res) => {
                 })
         }
 
-        const existUsers = await userDB.findOne({ userId })
+        const existUsers = await userDB.findOne({ userId });
         if (existUsers) {
             return res
                 .status(400)
-                .send({ errorMessage: '중복된 아이디입니다.' })
+                .send({ errorMessage: '중복된 아이디입니다.' });
         };
 
         const existNickname = await userDB.findOne({ nickName })
@@ -66,30 +67,60 @@ exports.signUp = async(req, res) => {
         if (existUsersEmail) {
             return res
                 .status(400)
-                .send({ errorMessage: '중복된 이메일입니다.' })
+                .send({ errorMessage: '중복된 이메일입니다.' });
         };
 
-        res.status(201).send({ message: '회원가입에 성공했습니다.' })
+        res.status(201).send({ message: '회원가입에 성공했습니다.' });
 
         const users = new userDB({ userId, email, nickName, userLocation, favorability, password, phoneNum })
         await users.save()
     } catch (err) {
         res.status(400).send({
             errorMessage: '요청한 데이터 형식이 올바르지 않습니다.',
-        })
-    }
-}
+        });
+    };
+};
+
+// 인증번호 메일로 보내기
+exports.sendMail = async(req, res) => {
+    const { email, nickName } = req.body; //회원가입시 입력한 정보 가져오기
+    let authNum = Math.random().toString().substring(2, 6); //랜덤한 숫자 4자리 생성
+
+    let emailParam = {
+        toEmail: email,
+
+        subject: '어디냥 인증번호 발급',
+
+        text: `
+        안녕하세요 ${nickName}님! 어디냥에서 인증번호 발급을 도와드릴게요!
+
+        ${nickName}님의 인증번호는 <  ${authNum}  > 입니다.
+
+        인증번호 입력란에 입력해 주세요! :)`
+
+    };
+
+    try {
+        mailer.sendEmail(emailParam);
+
+        res.status(200).send({ msg: `${nickName}님에게 메일 보내기 성공!`, authNum });
+    } catch (error) {
+        res.status(500).send({ errorMessage: '메세지 전송 싪패!' });
+    };
+};
+
+
 
 //로그인
 exports.login = async(req, res) => {
-    const { userId, password } = req.body
-    const user = await userDB.findOne({ userId })
+    const { userId, password } = req.body;
+    const user = await userDB.findOne({ userId });
     try {
         if (!user) {
-            return res.status(400).send({ errorMessage: '회원정보가 없습니다!' })
+            return res.status(400).send({ errorMessage: '회원정보가 없습니다!' });
         }
 
-        const userCompared = await bcrypt.compare(password, user.password)
+        const userCompared = await bcrypt.compare(password, user.password);
         if (!userCompared) {
             return res
                 .status(400)
@@ -100,12 +131,12 @@ exports.login = async(req, res) => {
         const token = jwt.sign({ authorId: user.authorId },
             process.env.SECRET_KEY, { expiresIn: '24h' } //토큰 24으로 유효시간 지정
         )
-        res.status(200).send({ message: `${userId}님이 로그인하셨습니다.`, token })
+        res.status(200).send({ message: `${userId}님이 로그인하셨습니다.`, token });
     } catch (err) {
         res.status(400).json({
             fail: '입력창을 확인 해주세요.',
         });
-    }
+    };
 };
 
 //카카오 로그인
