82e497f0805697aeb73a1b9216b6a711a9fe2bdc
xoxokss <sangseon14@gmail.com>
2022-06-30 23:20:31 +0900

    REFACTOR : 코드 리팩토링
    라우터 폴더에 index.js 추가.

---
+++ router/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    res.send('Backend server API page');
+});
+
+router.use('/users', require('./userRouter'));
+router.use('/auth', require('./socialRouter'));
+router.use('/post', require('./postRouter'));
+
+module.exports = router;
