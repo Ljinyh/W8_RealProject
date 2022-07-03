7615019b84ef26c996498f7863583e515a852cf4
jinhui_LEE <105095093+Ljinyh@users.noreply.github.com>
2022-07-03 21:40:20 +0900

    오탈자 수정 및 소셜로그인 res수정

--- models/mail.js
+++ models/mail.js
@@ -5,24 +5,24 @@ const mailSender = {
     sendEmail: function(param) {
         const transporter = nodemailer.createTransport({
             service: 'gmail', // 메일 보내는 곳
-            prot: 587,
+            port: 587,
             host: 'smtp.gmlail.com',
             secure: false,
             requireTLS: true,
             auth: {
                 user: senderInfo.user, // 보내는 메일의 주소
-                pass: senderInfo.pass // 보내는 메일의 비밀번호
-            }
+                pass: senderInfo.pass, // 보내는 메일의 비밀번호
+            },
         });
         // 메일 옵션
         const mailOptions = {
             from: senderInfo.user, // 보내는 메일의 주소
             to: param.toEmail, // 수신할 이메일
             subject: param.subject, // 메일 제목
-            text: param.text // 메일 내용
+            text: param.text, // 메일 내용
         };
 
-        // 메일 발송    
+        // 메일 발송
         transporter.sendMail(mailOptions, function(error, info) {
             if (error) {
                 console.log(error);
@@ -30,7 +30,7 @@ const mailSender = {
                 console.log('Email sent: ' + info.response);
             }
         });
-    }
+    },
 };
 
 module.exports = mailSender;
