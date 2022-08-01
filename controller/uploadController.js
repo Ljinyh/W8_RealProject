6c5e7eadef1a78afc9d048278da44f7540d3fdef
xoxokss <sangseon14@gmail.com>
2022-08-01 17:16:28 +0900

    Revert "Revert "이미지 업로더 코드 변경""
    This reverts commit ed3072e640606c8d19f25fa9cad17ac4283b13de.

--- controller/uploadController.js
+++ controller/uploadController.js
@@ -10,11 +10,11 @@ module.exports = {
         try {
             const image = req.files;
             const path = image.map((img) => img.location); // map 함수 사용. 여러개의 업로드 파일 URL을 배열로 출력.
-            // if (path.length===0) {
-            //     return res
-            //         .status(400)
-            //         .send({ message: '이미지가 업로드 되지 않았습니다.' });
-            // }
+            if (image === undefined) {
+                return res
+                    .status(400)
+                    .send({ message: '이미지가 존재하지 않습니다.' });
+            }
             res.status(200).send({
                 message: '업로드 요청 완료',
                 imageCount: image.length + '개',
@@ -44,19 +44,22 @@ module.exports = {
     // multipart-form data /image/multi
     multiImages: async (req, res) => {
         const image = req.files;
-        const firstImg = image.image[0];
-        const secondImg = image.images;
-        const secondPath = secondImg.map((img) => img.location); // map 함수 사용. 여러개의 업로드 파일 URL을 배열로 출력.
+        const imgArray = []
+        if(image.img1){imgArray.push(image.img1[0].location)}
+        if(image.img2){imgArray.push(image.img2[0].location)}
+        if(image.img3){imgArray.push(image.img3[0].location)}
+        if(image.img4){imgArray.push(image.img4[0].location)}
+        if(image.img5){imgArray.push(image.img5[0].location)}
+
         if (image === undefined) {
             return res
                 .status(400)
                 .send({ message: '이미지가 존재하지 않습니다.' });
         }
-        const path = {
-            firstImg: firstImg.location,
-            secondImg: secondPath,
-        };
-        res.status(200).send({ message: '업로드 성공', imgUrl: path });
+        // const path = {
+        //     img1,img2,img3,img4,img5
+        // };
+        res.status(200).send({ message: '업로드 성공', imgUrl: imgArray });
     },
 };
 
@@ -74,19 +77,19 @@ app.post('/profile', upload.single('avatar'), function (req, res, next) {
     // 텍스트 필드가 있는 경우, req.body가 이를 포함할 것입니다.
   })
   
-  app.post('/photos/upload', upload.array('photos', 12), function (req, res, next) {
+  app.post('/photos/upload', upload.imgArray('photos', 12), function (req, res, next) {
     // req.files 는 `photos` 라는 파일정보를 배열로 가지고 있습니다.
     // 텍스트 필드가 있는 경우, req.body가 이를 포함할 것입니다.
   })
   
   const cpUpload = upload.fields([{ name: 'avatar', maxCount: 1 }, { name: 'gallery', maxCount: 8 }])
   app.post('/cool-profile', cpUpload, function (req, res, next) {
-    // req.files는 (String -> Array) 형태의 객체 입니다.
+    // req.files는 (String -> imgArray) 형태의 객체 입니다.
     // 필드명은 객체의 key에, 파일 정보는 배열로 value에 저장됩니다.
     //
     // e.g.
     //  req.files['avatar'][0] -> File
-    //  req.files['gallery'] -> Array
+    //  req.files['gallery'] -> imgArray
     //
     // 텍스트 필드가 있는 경우, req.body가 이를 포함할 것입니다.
   })
